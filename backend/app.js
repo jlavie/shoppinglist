710b19e0b373f19932e6e23fe27ac7f1a9e90e1c
petitlardon <lavie.jul@gmail.com>
2025-01-19 13:57:08 +0100

    init project

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+// importation d'express
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/dev')
+.then(() => console.log('Connection à MongoDB réussie'))
+.catch(() => console.log('Connection à MongoDB échouée'))
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.send("test")
+    console.log('test')
+})
+
+module.exports = app;
