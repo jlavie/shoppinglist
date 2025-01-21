48ae5bb0b48a858c706a3f133f66ef9b86479aac
petitlardon <lavie.jul@gmail.com>
2025-01-21 22:47:51 +0100

    add material angular

--- frontend/shoppinglist/src/app/ingredients/ingredient-new/ingredient-new.component.ts
+++ frontend/shoppinglist/src/app/ingredients/ingredient-new/ingredient-new.component.ts
@@ -3,10 +3,14 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { IngredientCategory } from '../ingredient.utils';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-ingredient-new',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatSelectModule, MatInputModule],
   templateUrl: './ingredient-new.component.html',
   styleUrl: './ingredient-new.component.css'
 })
