d21e668fa1da50608ef228a6c9beb80dc2fd02df
petitlardon <lavie.jul@gmail.com>
2025-01-24 00:30:39 +0100

    configure routing

--- frontend/shoppinglist/src/app/app.routes.ts
+++ frontend/shoppinglist/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { IngredientItemComponent } from './ingredients/ingredient-item/ingredient-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: IngredientsComponent
+    },
+    {
+        path: 'ingredient/:id',
+        component: IngredientItemComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
