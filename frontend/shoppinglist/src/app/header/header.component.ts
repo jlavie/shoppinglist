24359c27233cdb277b527ee91fca84367f8c0465
petitlardon <lavie.jul@gmail.com>
2025-02-04 21:32:49 +0100

    update header

--- frontend/shoppinglist/src/app/header/header.component.ts
+++ frontend/shoppinglist/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginService } from '../pages/login/login.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,5 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  loginService = inject(LoginService);
 }
